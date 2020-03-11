22685622dc4c999a1bd51c5d8407215a5cf102a9
Nathan Ladd <nathanladd@gmail.com>
2020-03-11 11:56:56 -0500

    .vuepress/config.js is added

---
+++ .vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: 'TestBench',
+  description: 'Design-oriented test framework for Ruby',
+  dest: './_build',
+  themeConfig: {
+    activeHeaderLinks: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Quick Start', link: '/#quick-start' },
+      {
+        text: 'Documentation', items: [
+          { text: 'Running Test Files', link: '/#running-test-files' },
+          { text: 'Writing Tests', link: '/#writing-tests' },
+          { text: 'Assertions', link: '/#assertions' },
+          { text: 'Block-Form Assertions', link: '/#block-form-assertions' },
+          { text: 'Fixtures', link: '/#fixtures' },
+        ]
+      },
+      {
+        text: 'Recipes',
+        link: '/Recipes',
+        items: [
+          { text: 'Running All Tests in a Single Directory', link: '/Recipes#running-all-tests-in-a-single-directory' },
+          { text: 'Using TestBench Without Monkey Patching', link: '/Recipes#using-testbench-without-monkey-patching' },
+          { text: 'Randomizing the Execution Order', link: '/Recipes#randomizing-the-execution-order' },
+          { text: 'Parallel Test Execution', link: '/Recipes#parallel-test-execution' },
+          { text: "Changing TestBench's Output Device", link: '/Recipes#changing-testbench-s-output-device' },
+          { text: 'Re-run Failed Tests', link: '/Recipes#re-run-failed-tests' },
+        ]
+      }
+    ]
+  }
+}
