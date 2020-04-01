d5ba279c4dd0a6a4d5f3f85dcf02e65cf23c73f4
Scott Bellware <sbellware@gmail.com>
2020-03-31 20:23:57 -0500

    Top level menu

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,26 +5,25 @@ module.exports = {
   themeConfig: {
     activeHeaderLinks: true,
     nav: [
-      { text: 'Home', link: '/' },
       {
-        text: 'Documentation', items: [
-          { text: 'Quick Start', link: '/Documentation#quick-start' },
-          { text: 'Running Test Files', link: '/Documentation#running-test-files' },
-          { text: 'Writing Tests', link: '/Documentation#writing-tests' },
-          { text: 'Assertions', link: '/Documentation#assertions' },
-          { text: 'Block-Form Assertions', link: '/Documentation#block-form-assertions' },
-          { text: 'Fixtures', link: '/Documentation#fixtures' },
+        text: 'Home', link: '/'
+      },
+      {
+        text: 'Philosophy', link: '/philosophy.md'
+      },
+      {
+        text: 'User Guide', items: [
+          { text: 'Getting Started', link: '/user-guide/getting-started.md/' },
+          { text: 'Writing Tests', link: '/user-guide/writing-tests.md/' },
+          { text: 'Running Tests', link: '/user-guide/running-tests.md/' },
+          { text: 'Fixtures', link: '/user-guide/fixtures.md/' },
+          { text: 'Recipes', link: '/user-guide/recipes.md/' }
         ]
       },
       {
-        text: 'Recipes',
-        items: [
-          { text: 'Running All Tests in a Single Directory', link: '/Recipes#running-all-tests-in-a-single-directory' },
-          { text: 'Using TestBench Without Monkey Patching', link: '/Recipes#using-testbench-without-monkey-patching' },
-          { text: 'Randomizing the Execution Order', link: '/Recipes#randomizing-the-execution-order' },
-          { text: 'Parallel Test Execution', link: '/Recipes#parallel-test-execution' },
-          { text: "Changing TestBench's Output Device", link: '/Recipes#changing-testbench-s-output-device' },
-          { text: 'Re-run Failed Tests', link: '/Recipes#re-run-failed-tests' },
+        text: 'Examples', items: [
+          { text: 'Quickstart', link: '/examples/quickstart.md' },
+          { text: 'Example Projects', link: '/examples/example-projects.md' }
         ]
       }
     ]
